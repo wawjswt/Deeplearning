25e6da94ab5dfa2d6fcbe72abfa91a1f6b96c2e4
Shen <SamMantos@outlook.com>
2025-08-26 12:01:05 +0800

    house数据

---
+++ 2.2数据预处理/读取数据集.py
@@ -0,0 +1,14 @@
+# 为了能用深度学习来解决现实世界的问题，我们经常从预处理原始数据开始， 而不是从那些准备好的张量格式数据开始。
+# 在Python中常用的数据分析工具中，我们通常使用pandas软件包。
+import pandas as pd
+import os
+import os
+
+os.makedirs(os.path.join('..', 'data'), exist_ok=True)
+data_file = os.path.join('..', 'data', 'house_tiny.csv')  # data_file='../data/house_tiny.csv'
+with open(data_file, 'w') as f:
+    f.write('NumRooms,Alley,Price\n')  # 列名
+    f.write('NA,Pave,127500\n')  # 每行表示一个数据样本
+    f.write('2,NA,106000\n')
+    f.write('4,NA,178100\n')
+    f.write('NA,NA,140000\n')
